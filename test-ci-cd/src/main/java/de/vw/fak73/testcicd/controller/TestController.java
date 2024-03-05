4a0b4cc0a58f13a72824213af3a8b6777beced3c
Kramer, Adriana (SE-A/35) <adri.kramer@gmail.com>
2024-03-05 15:43:25 +0100

    Automatically updated.

--- test-ci-cd/src/main/java/de/vw/fak73/testcicd/controller/TestController.java
+++ test-ci-cd/src/main/java/de/vw/fak73/testcicd/controller/TestController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test() {
-        return "Now I was refreshed automatically";
+        return "Changed yet again automatically";
     }
 }
